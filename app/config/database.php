67488c9e80d54ff0e88b41d4d4ff48677421cfd2
brs <brs@brs-Studio-1557.(none)>
2014-04-04 17:39:49 -0300

    change database configuration

--- app/config/database.php
+++ app/config/database.php
@@ -64,10 +64,10 @@ return array(
 		),*/
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '$_SERVER["DB1_HOST"]',
-			'database'  => '$_SERVER["DB1_NAME"]',
-			'username'  => '$_SERVER["DB1_USER"]',
-			'password'  => '$_SERVER["DB1_PASS"]',
+			'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+			'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'dengoDB',
+			'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'root',
+			'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
