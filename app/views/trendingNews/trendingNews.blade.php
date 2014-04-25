2189c3f3462b3156a901ca4ab7218e04ef306d1f
Sebastian Quintana <seba@seba.(none)>
2014-04-25 11:33:21 -0300

    Add the option of click in the image and search the trendingNews

--- app/views/trendingNews/trendingNews.blade.php
+++ app/views/trendingNews/trendingNews.blade.php
@@ -19,13 +19,15 @@
  @foreach($tenTrendingNews as $trendingNews) 
   <div class="col-sm-6 col-md-6">
     <div class="thumbnail">
-       @if ($trendingNews->image == null)
-            <img class='img-rounded newsImage' src="images/icono.ico">
-       @else
-            <img class='img-rounded newsImage' src="{{ $trendingNews->image }}">
-       @endif
+        <a class="dengoDelDiaNews" href="/{{{ $trendingNews->title }}}">
+         @if ($trendingNews->image == null)
+              <img class='img-rounded newsImage' src="images/icono.ico">
+         @else 
+              <img class='img-rounded newsImage' src="{{ $trendingNews->image }}">
+         @endif 
+       </a>
       <div class="caption">
-        <h3><a class="dengoDelDiaNews" href="/{{{ $trendingNews->title }}}">{{ $trendingNews->title }}</h3>
+        <h3><a class="dengoDelDiaNews" href="/{{{ $trendingNews->title }}}">{{ $trendingNews->title }}</a></h3>
       </div>
     </div>
   </div>
