8f897886014ead2eac3623f0d757d8c45a3aec39
sebquintana <seba@seba.(none)>
2015-02-24 19:24:28 -0300

    new begining

--- app/views/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="modal-footer">
-	<div class="span12">
-		<p>&copy; 2013 - Dengo 	Contacto: dengo.web@gmail.com</p>
-	</div>
-</div>
--- app/views/includes/head.blade.php
+++
@@ -1,8 +0,0 @@
-<meta charset="utf-8">
-<title>Dengo</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
-<meta name="author" content="">
-
-{{ HTML::style('css/bootstrap.min-new.css'); }}
-{{ HTML::style('css/dengo.css'); }}
